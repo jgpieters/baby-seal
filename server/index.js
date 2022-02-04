d4d2fa7b2d92409ac5bc2c4cc40576f2aa1318e3
Joris Pieters <joris.pieters@rabobank.nl>
2022-02-04 20:22:09 +0100

    fix: small fixes for pipeline

--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const port = process.env.PORT || 8080;
+
 app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.get("/", (req, res) => {
+  console.log("express route to serve webiste");
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-app.listen(8080);
+app.get("/test", (req, res) => {
+  console.log("express route to serve website");
+  res.send("nothing to see here, go away!");
+});
+
+app.listen(port);
