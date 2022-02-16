da314e44b88f65cfc13a8ae4dc8390dc1d934f82
Joris Pieters <joris.pieters@rabobank.nl>
2022-02-16 21:12:30 +0100

    feat: update footer

--- src/components/FooterComponent/FooterComponent.tsx
+++ src/components/FooterComponent/FooterComponent.tsx
@@ -6,6 +6,7 @@ import Container from "react-bootstrap/Container";
 import LinkComponent from "../LinkComponent/LinkComponent";
 import styles from "./FooterComponent.module.scss";
 import { Footer } from "../../model/model";
+import { SocialIcon } from "react-social-icons";
 
 interface FooterComponentProps {
   data: Footer;
@@ -17,10 +18,10 @@ const FooterComponent: FC<FooterComponentProps> = (props) => {
     <div className={styles.FooterComponent} data-testid="FooterComponent">
       <Container>
         <Row>
-          <Col xs="12" md="6" lg="4">
+          <Col xs="12" md="6" lg="4" className={styles.menu}>
             <h2>Menu</h2>
             {links?.map((link, index) => (
-              <div className={styles.LinkComponent} key={index}>
+              <div className={styles.linkContainer} key={index}>
                 <LinkComponent data={link}></LinkComponent>
               </div>
             ))}
@@ -38,6 +39,18 @@ const FooterComponent: FC<FooterComponentProps> = (props) => {
               <LinkComponent data={contact?.website}></LinkComponent>
             </div>
             <h2>Follow us on</h2>
+            <SocialIcon
+              className={styles.icon}
+              url="https://twitter.com/babyseal"
+            />
+            <SocialIcon
+              className={styles.icon}
+              url="https://instagram.com/babyseal"
+            />
+            <SocialIcon
+              className={styles.icon}
+              url="https://facebook.com/babyseal"
+            />
           </Col>
         </Row>
       </Container>
